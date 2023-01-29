cdeb18e2355573aa71f9eb6550022216216cc8ef
Jacob <iam@jacobjohns.com>
2023-01-29 02:41:22 -0600

    main page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 //import App from './App';
 import Navbar from './components/Navbar';
+import Main from './components/Main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
+    <Main />
   </React.StrictMode>
 );
 
