d24b7d8c8fcd4d708797cb17cd0aa42934bb16cc
Jacob <iam@jacobjohns.com>
2023-01-29 04:15:49 -0600

    hamburgler time

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,12 @@ function Main() {
             <h2>Hey I guess!</h2>
             <section>
                 <p>
+                    I figured it out... Hamburgler time.
+                </p>
+                <p className="App-signature">Me @ 1/29/23</p>
+            </section>
+            <section>
+                <p>
                     I can't do CSS though! I suck...
                 </p>
                 <p className="App-signature">Me @ 1/29/23</p>
